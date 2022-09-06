f0eaec9d1657a276cce8c5908f62b12fffacdb74
GabrielCarvalhoM <c.gabrielmaciel@icloud.com>
2022-09-06 20:09:33 -0300

    Initial Commit

---
+++ AppComprasUSA/AppComprasUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppComprasUSAApp.swift
+//  AppComprasUSA
+//
+//  Created by Gabriel Carvalho on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AppComprasUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
